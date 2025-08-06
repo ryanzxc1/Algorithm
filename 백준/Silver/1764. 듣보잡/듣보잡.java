fa5615f6f7a7688e4a125a9a9e2c386f4ac1bbd6
ryanzxc1 <93314866+ryanzxc1@users.noreply.github.com>
2025-08-06 15:42:33 +0900

    [Silver IV] Title: 듣보잡, Time: 276 ms, Memory: 26360 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,95 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st1= new StringTokenizer(br.readLine());
+        int N= Integer.parseInt(st1.nextToken());
+        int M = Integer.parseInt(st1.nextToken());
+        
+        HashSet<String> stringN=new HashSet<>();
+        ArrayList<String> list = new ArrayList<>();
+        
+        
+        for(int i=0;i<N;i++) {
+        		stringN.add(br.readLine());
+        }
+        
+    	for(int i=0;i<M;i++) {
+    		String name= br.readLine();
+    		if(stringN.contains(name)) {
+    			list.add(name);
+    		}
+    	
+    	}
+        
+       
+        Collections.sort(list);
+       
+        
+        System.out.println(list.size());
+        for(int i=0;i<list.size();i++) {
+        	System.out.println(list.get(i));
+        }
+        br.close();
+    }
+}
+
+/*
+ * 
+ * 시간초과 코드
+  import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st1= new StringTokenizer(br.readLine());
+        int N= Integer.parseInt(st1.nextToken());
+        int M = Integer.parseInt(st1.nextToken());
+        int cnt=0;
+        
+        
+        String[] stringN = new String[N];
+        String[] stringM = new String[M];
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=0;i<N;i++) {
+        	stringN[i]=br.readLine();
+        	
+        	}
+        
+    	for(int i=0;i<M;i++) {
+    		stringM[i]=br.readLine();
+    	
+    	}
+        
+        for(int i=0;i<N;i++) {
+        	for(int j=0;j<M;j++) {
+        		if(stringN[i].equals(stringM[j])) {
+        			list.add(stringM[j]);
+        			cnt++;
+        		}
+        	}
+        }
+        Collections.sort(list);
+       
+        
+        System.out.println(cnt);
+        for(int i=0;i<list.size();i++) {
+        	System.out.println(list.get(i));
+        }
+        br.close();
+    }
+}
+ */
