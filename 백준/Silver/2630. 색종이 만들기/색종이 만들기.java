cf6ea08a755d780244ed4259166148b4182b825c
ryanzxc1 <93314866+ryanzxc1@users.noreply.github.com>
2025-08-10 18:28:15 +0900

    [Silver II] Title: 색종이 만들기, Time: 136 ms, Memory: 15584 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int numW = 0; 
+    static int numB = 0; 
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        checkWB(0, 0, N);
+
+        System.out.println(numW);
+        System.out.println(numB);
+    }
+
+    
+    public static void checkWB(int x, int y, int size) {
+        
+        if (isAllSameColor(x, y, size)) {
+            if (map[x][y] == 0) {
+                numW++; 
+            } else {
+                numB++; 
+            }
+            return; //재귀함수 멈추게 하는 요소
+        }
+
+        
+        int newSize = size / 2;
+        
+        checkWB(x, y, newSize); 
+        checkWB(x, y + newSize, newSize); 
+        checkWB(x + newSize, y, newSize); 
+        checkWB(x + newSize, y + newSize, newSize);
+    }
+
+    
+    public static boolean isAllSameColor(int x, int y, int size) {
+        int firstColor = map[x][y];
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (map[i][j] != firstColor) {
+                    return false;
+                }
+            }
+        }
+        return true; 
+    }
+}
