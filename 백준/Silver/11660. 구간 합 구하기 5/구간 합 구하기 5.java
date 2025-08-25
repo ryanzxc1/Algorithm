7691aae2c91ff09ef4e453bc283ab1469cc8b07e
ryanzxc1 <93314866+ryanzxc1@users.noreply.github.com>
2025-08-25 15:00:05 +0900

    Update 구간 합 구하기 5.java

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,49 +1,45 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        int[][] arr = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int[][] prefixSum = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
-            }
-        }
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-
-            int result = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
-            sb.append(result+"\n");
+        List<List<Integer>> arr = new ArrayList<>();
+        int[][] prefixSum = new int[N+1][N+1];
+        for(int i=0;i<N;i++) {
+        	List<Integer> list = new ArrayList<>();
+        	st = new StringTokenizer(br.readLine());
+        	for(int j=0;j<N;j++)
+        		list.add(Integer.parseInt(st.nextToken()));
+        	arr.add(list);
+        }
+        for(int i=1;i<=N;i++) {
+        	for(int j=1;j<=N;j++) {
+        		prefixSum[i][j] = arr.get(i-1).get(j-1) + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+        	}
         }
         
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        for(int i=0;i<M;i++) {
+        	
+        	st = new StringTokenizer(br.readLine());
+        	int x1 = Integer.parseInt(st.nextToken());
+        	int y1 = Integer.parseInt(st.nextToken());
+        	int x2 = Integer.parseInt(st.nextToken());
+        	int y2 = Integer.parseInt(st.nextToken());
+        	
+        	
+        	int result = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+        	sb.append(result+"\n");
+        }
+        System.out.println(sb);
         br.close();
     }
 }
